59db1c1335b62956f4da33585c33b3715bd3768f
Cleginaldo <cleginaldo3000@gmail.com>
2022-06-05 18:21:49 -0300

    feat: fix screeshot

--- api/src/useCases/SubmitFeedbackUseCase.ts
+++ api/src/useCases/SubmitFeedbackUseCase.ts
@@ -30,12 +30,11 @@ export class SubmitFeedbackUseCase {
     await this.mailProvider.sendMail({
       subject: "Novo feedback",
       body: [
-        `
-      <div style="font-family: sans-serif; font-size: 16px; color: #111;">
-      <p>Tipo do feedback: ${type}<p/>`,
-        `<p>Comentário: ${comment}<p/>
-      </div>
-      `,
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+        `<p>Tipo do feedback: ${type}<p/>`,
+        `<p>Comentário: ${comment}<p/>`,
+        screenshot ? `<img src="${screenshot}" />` : ``,
+        `</div>`,
       ].join("\n"),
     });
   }
